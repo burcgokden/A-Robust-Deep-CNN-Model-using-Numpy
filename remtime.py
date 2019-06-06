72207b939e320b957c04c1a80bcd06b42bb63bce
Burc Gokden <50996809+burcgokden@users.noreply.github.com>
2019-06-06 03:39:47 -0700

    Updated return value

--- remtime.py
+++ remtime.py
@@ -3,4 +3,4 @@ def printTime(remtime):
 	hrs = int(remtime)/3600
 	mins = int((remtime/60-hrs*60))
 	secs = int(remtime-mins*60-hrs*3600)
-	print("########  "+str(hrs)+"Hrs "+str(mins)+"Mins "+str(secs)+"Secs remaining  ########")
+	return "########  "+str(hrs)+"Hrs "+str(mins)+"Mins "+str(secs)+"Secs remaining  ########"
